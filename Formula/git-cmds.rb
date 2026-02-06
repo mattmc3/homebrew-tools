1e78bc617aebae14a7475183afa732b254ce9b6b
mattmc3 <mattmc3@gmail.com>
2026-02-06 17:29:13 -0500

    Add tag info

--- Formula/git-cmds.rb
+++ Formula/git-cmds.rb
@@ -1,8 +1,11 @@
 class GitCmds < Formula
   desc "Extra commands for git - cloner, browse, whoami, and more"
   homepage "https://github.com/mattmc3/git-cmds"
-  head "https://github.com/mattmc3/git-cmds.git", branch: "main"
+  url "https://github.com/mattmc3/git-cmds/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f9087b60ec5ccb6ef31bc81ecebb32de6834edaacb618b0af589cd35341ffe13"
   license "MIT"
+
+  head "https://github.com/mattmc3/git-cmds.git", branch: "main"
 
   def install
     bin.install Dir["bin/git-*"]
